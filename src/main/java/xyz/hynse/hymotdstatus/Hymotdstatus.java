7439217e579384f0a5181578bb50442239f69aec
MidnightTale <midnighttale24@gmail.com>
2023-09-02 05:23:41 +0700

    Initial commit

---
+++ src/main/java/xyz/hynse/hymotdstatus/Hymotdstatus.java
@@ -0,0 +1,17 @@
+package xyz.hynse.hymotdstatus;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class Hymotdstatus extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
