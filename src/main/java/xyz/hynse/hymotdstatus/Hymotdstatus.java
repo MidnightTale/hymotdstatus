e4d0a13c569b18aa8bed8aa66e7a68db7c2c90af
MidnightTale <midnighttale24@gmail.com>
2024-05-19 10:50:28 +0700

    is not work

--- src/main/java/xyz/hynse/hymotdstatus/Hymotdstatus.java
+++ src/main/java/xyz/hynse/hymotdstatus/Hymotdstatus.java
@@ -3,6 +3,7 @@ package xyz.hynse.hymotdstatus;
 import net.kyori.adventure.text.Component;
 import net.kyori.adventure.text.format.NamedTextColor;
 import org.bukkit.Bukkit;
+import org.bukkit.ChatColor;
 import org.bukkit.World;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
@@ -31,19 +32,23 @@ public final class Hymotdstatus extends JavaPlugin implements Listener {
         String rainEmoji = "\uD83C\uDF27"; // Rain emoji
         String cloudEmoji = "\u2601"; // Cloud emoji
         String weathericon = isStormy ? thunderEmoji : (world.isClearWeather() ? cloudEmoji : rainEmoji);
-        NamedTextColor lineColor = NamedTextColor.WHITE;
-        NamedTextColor playColor = ColorUtil.fromHexString("#4454dd");
-        NamedTextColor boldColor = ColorUtil.fromHexString("#ffc5ff");
-        NamedTextColor xyzColor = ColorUtil.fromHexString("#b289b2");
-        NamedTextColor resetColor = NamedTextColor.WHITE;
-        String playText = "  \u1D18\u029F\u1D00\u028F"; // "play"
-        String xyzText = "x\u028F\u1D22  "; // "xyz"
+        String line2color = String.valueOf(net.md_5.bungee.api.ChatColor.of("#ffffff"));
+        String line1_0color = String.valueOf(net.md_5.bungee.api.ChatColor.of("#4454dd"));
+        String line1_1color = String.valueOf(net.md_5.bungee.api.ChatColor.of("#ffc5ff"));
+        String line1_2color = String.valueOf(net.md_5.bungee.api.ChatColor.of("#b289b2"));
+        String bold = ChatColor.BOLD.toString();
+        String italic = ChatColor.ITALIC.toString();
+        String reset = ChatColor.RESET.toString();
+        String strikethrough = ChatColor.STRIKETHROUGH.toString();
+        String playText = "  \u1d18\u029f\u1d00\u028f"; // "play"
+        String xyzText = "x\u028f\u1d22  "; // "xyz"
+        String square = "\u2588";
         String spacerL = "                \u2192";
         String spacerR = "\u2190                 ";
 
         String timeString = String.format("%02d:%02d", hours, minutes);
-        String formattedLine1 = spacerR + resetColor + playColor + playText + boldColor + "HYNSE" + xyzColor + xyzText + playColor + spacerL + resetColor;
-        String formattedLine2 = lineColor + timeicon + " Day: " + dayCount + " - " + weathericon + " Time: " + timeString;
+        String formattedLine1 = line1_0color + strikethrough + spacerL + reset + line1_2color + playText + "." + line1_1color + bold + "HYNSE" + reset + line1_2color + "." + xyzText + line1_0color + strikethrough + spacerR + reset;
+        String formattedLine2 = line2color + timeicon + " Day: " + dayCount + " - " + weathericon + " Time: " + timeString;
 
         // Center-align both lines
         formattedLine1 = centerAlign(formattedLine1);
